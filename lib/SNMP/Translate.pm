c2a2360be44d949ab47f78fae78368154cd12d32
Robert Drake <rdrake@direcpath.com>
2015-12-30 00:17:37 -0500

    updated docs and copyright notice

--- lib/SNMP/Translate.pm
+++ lib/SNMP/Translate.pm
@@ -38,15 +38,23 @@ $verbose = 0;   # controls warning/info output of SNMP module,
 $best_guess = 0;  # determine whether or not to enable best-guess regular
                   # expression object name translation.  1 = Regex (-Ib),
 		  # 2 = random (-IR)
+
+=head2 translateObj
+
+    my $oid = translateObj('ifDescr');
+
+Translate object identifier(tag or numeric) into alternate representation
+(i.e., sysDescr => '.1.3.6.1.2.1.1.1' and '.1.3.6.1.2.1.1.1' => sysDescr)
+when $SNMP::Translate::use_long_names or second arg is non-zero the translation will
+return longer textual identifiers (e.g., system.sysDescr).  An optional
+third argument of non-zero will cause the module name to be prepended
+to the text name (e.g. 'SNMPv2-MIB::sysDescr').  If no Mib is loaded
+when called and $SNMP::Translate::auto_init_mib is enabled then the Mib will be
+loaded. Will return 'undef' upon failure.
+
+=cut
+
 sub translateObj {
-# Translate object identifier(tag or numeric) into alternate representation
-# (i.e., sysDescr => '.1.3.6.1.2.1.1.1' and '.1.3.6.1.2.1.1.1' => sysDescr)
-# when $SNMP::Translate::use_long_names or second arg is non-zero the translation will
-# return longer textual identifiers (e.g., system.sysDescr).  An optional
-# third argument of non-zero will cause the module name to be prepended
-# to the text name (e.g. 'SNMPv2-MIB::sysDescr').  If no Mib is loaded
-# when called and $SNMP::Translate::auto_init_mib is enabled then the Mib will be
-# loaded. Will return 'undef' upon failure.
    SNMP::Translate::init_snmp("perl");
    my $obj = shift;
    my $temp = shift;
@@ -67,6 +75,15 @@ sub translateObj {
    return($res);
 }
 
+=head2 translate
+
+    my $result = translate([ 'ifDescr', 'ifInOctets' ]);
+
+This will translate an arrayref or list of objects into their OIDs or
+translate OIDs into their names.  Returns an arrayref.
+
+=cut
+
 sub translate {
     [ map { translateObj($_) } ref($_[0]) eq 'ARRAY' ?  @{$_[0]} : @_ ];
 }
@@ -76,9 +93,13 @@ sub translate {
 
 =head1 AUTHOR
 
-bugs, comments, questions to net-snmp-users@lists.sourceforge.net
+bugs, comments, questions to rdrake@cpan.org
 
 =head1 Copyright
+
+Most of the source code is directly copied from the SNMP.pm/xs modules.  I am
+leaving their copyright notice intact but please direct complaints, feature
+requests or whatever to me.
 
      Copyright (c) 1995-2000 G. S. Marzot. All rights reserved.
      This program is free software; you can redistribute it and/or
